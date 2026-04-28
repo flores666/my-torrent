7cd9f71853f225fe27b08e6199873fd77b11c0b5
e-vasilyev <e-vasilyev@asoft.local>
2026-04-28 15:48:59 +0300

    refactor + debian torrent parsing

--- cmd/my-torrent/main.go
+++ cmd/my-torrent/main.go
@@ -1,16 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"my-torrent/internal/bencode"
+	"os"
 )
 
 func main() {
-	res, err := bencode.Decode([]byte("d8:announce26:http://tracker.example.com4:infod6:lengthi12345e4:name11:example.txt12:piece lengthi16384e6:pieces20:abcdefghijklmnopqrste4:listld3:foo3:bared3:numi42eeee"))
-	//res, err := bencode.Decode([]byte("ld3:foo3:bared3:numi42eee"))
+	fmt.Println("Opening torrent file")
+
+	file, err := os.Open("debian.torrent")
 	if err != nil {
-		fmt.Printf("Error - %s\n", err)
+		log.Fatal(err)
 	}
 
-	bencode.PrintParsed(res)
+	defer file.Close()
+
+	fmt.Println("Reading torrent file")
+
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := make([]byte, stat.Size())
+	count, err := file.Read(data)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Decoding torrent file, got %d bytes\n", count)
+
+	res, err := bencode.Decode(data[:count])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bytes, err := json.Marshal(res)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.WriteFile("debian.json", bytes, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
